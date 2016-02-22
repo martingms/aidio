12f9b1584e50cc67d797b338a44db0aa6396081a
Martin Gammelsæter <martin@mg.am>
2016-02-22 23:51:33 +0100

    Make player2.py more usable from scripts.

--- player2.py
+++ player2.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 import io
+import sys
 import functools
 
 import midi.files
-from synth.players import play_midifile
+from synth.utils import play_midifile
 from synth.sinks import write_pcm
 from synth.generators import sine, square
 from synth.instruments import MonoSynth
 
-with io.open('data/smb1-Theme.mid', 'rb') as f:
+if len(sys.argv) < 2:
+    print('usage: {} <midifile>'.format(sys.argv[0]))
+    sys.exit(1)
+
+with io.open(sys.argv[1], 'rb') as f:
     mf = midi.files.load(f)
 
 instruments = {
@@ -16,5 +21,4 @@ instruments = {
     2: MonoSynth(square),
 }
 
-with io.open('test2.pcm', 'wb') as out:
-    write_pcm(out, play_midifile(mf, instruments))
+write_pcm(sys.stdout.buffer, play_midifile(mf, instruments))
